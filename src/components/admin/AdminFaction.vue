3b64edbccf0208088d2be031f7b6aaa1342e8766
yyx219 <yyx219@qq.com>
2018-11-02 23:25:26 +0800

    修复了门派注册页面的验证问题

--- src/components/admin/AdminFaction.vue
+++ src/components/admin/AdminFaction.vue
@@ -17,10 +17,10 @@
             <el-row type="flex" class="row-bg" justify="space-around">
               <el-col :span="24">
                 <el-form :model="faction" :rules="rules" ref="faction" label-width="100px" class="demo-ruleForm">
-                  <el-form-item label="门派名称" prop="name">
+                  <el-form-item label="门派名称" prop="className">
                     <el-input v-model="faction.className"></el-input>
                   </el-form-item>
-                  <el-form-item label="掌门设置" prop="region">
+                  <el-form-item label="掌门设置" prop="classUserID">
                     <el-select v-model="faction.classUserID" placeholder="请选择门派负责人">
                       <el-option label="王昕" value="王昕"></el-option>
                       <el-option label="王雪晶" value="王雪晶"></el-option>
@@ -28,21 +28,21 @@
                   </el-form-item>
                   <el-form-item label="创建时间" required>
                     <el-col :span="11">
-                      <el-form-item prop="date1">
-                        <el-date-picker type="date" placeholder="选择日期" v-model="faction.date1" style="width: 100%;"></el-date-picker>
+                      <el-form-item prop="date">
+                        <el-date-picker type="date" placeholder="选择日期" v-model="faction.date" style="width: 100%;"></el-date-picker>
                       </el-form-item>
                     </el-col>
                     <el-col class="line" :span="2" style="text-align:center">-</el-col>
                     <el-col :span="11">
-                      <el-form-item prop="date2">
-                        <el-time-picker type="fixed-time" placeholder="选择时间" v-model="faction.date2" style="width: 100%;"></el-time-picker>
+                      <el-form-item prop="time">
+                        <el-time-picker type="fixed-time" placeholder="选择时间" v-model="faction.time" style="width: 100%;"></el-time-picker>
                       </el-form-item>
                     </el-col>
                   </el-form-item>
-                  <el-form-item label="是否开放" prop="delivery">
+                  <el-form-item label="是否开放" prop="isClassOpen">
                     <el-switch v-model="faction.isClassOpen"></el-switch>
                   </el-form-item>
-                  <el-form-item label="门派性质" prop="type">
+                  <el-form-item label="门派性质" prop="classNature">
                     <el-radio-group v-model="faction.classNature">
                       <el-radio label="魔教" name="type"></el-radio>
                       <el-radio label="正教"  name="type"></el-radio>
@@ -50,8 +50,8 @@
                       <el-radio label="锦衣卫" name="type"></el-radio>
                     </el-radio-group>
                   </el-form-item>
-                  <el-form-item label="门派介绍" prop="desc">
-                    <el-input type="textarea" :rows=6 v-model="faction.desc"></el-input>
+                  <el-form-item label="门派介绍" prop="classInfo">
+                    <el-input type="textarea" :rows=6 v-model="faction.classInfo"></el-input>
                   </el-form-item>
                   <el-form-item>
                     <el-button type="primary" @click="submitForm('faction')">创建门派</el-button>
